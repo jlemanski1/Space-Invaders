a549188d97fcb562278abcc314dbc9169a76799c
Jonathan L <jlemanski1@gmail.com>
2018-04-21 21:41:49 -0600

    Created Sound Manager & Player controller

---
+++ Space Invaders/Assets/Scripts/ShipController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ShipController : MonoBehaviour {
+
+    [SerializeField]
+    private int speed;
+
+    //Public GameObject bullet;
+
+    
+
+    private void Start () {
+        speed = 30;
+	}
+
+    // Spaceship movement
+    private void FixedUpdate() {
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalMove, 0) * speed;
+    }
+
+    private void Update() {
+        if (Input.GetButtonDown("Jump")) {
+            // Fire bullet
+            // Bullet Sound effect
+        }
+    }
+
+}
---
+++ Space Invaders/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager Instance = null;
+
+    public AudioClip alienBuzz1;
+    public AudioClip alienBuzz2;
+    public AudioClip alienDeath;
+    public AudioClip bulletFire;
+    public AudioClip shipExplosion;
+
+    private AudioSource soundEffectAudio;
+
+    private void Start () {
+        // Singleton Pattern
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+
+        soundEffectAudio = GetComponent<AudioSource>();
+	}
+	
+    // Play sound effect
+	public void PlayOneShot(AudioClip clip) {
+        soundEffectAudio.PlayOneShot(clip);
+    }
+}
